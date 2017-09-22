1aac561c4056145e28416111935e1e6af70576a8
shreya suthar <sutharshreya0@gmail.com>
2017-09-22 10:03:39 +0530

    my second commit

---
+++ caller.rb
@@ -0,0 +1,7 @@
+require_relative 'targaryen'
+
+def caller
+    
+        obj1=targaryen.new
+        obj1.gitDemo
+end
---
+++ targaryen.rb
@@ -0,0 +1,13 @@
+require_relative 'stark'
+class GitSub
+  
+  def gitDemo
+    
+      obj1 = GitMain.new
+      obj1.method
+    
+  end
+end
+
+obj2=GitSub.new
+obj2.gitDemo
